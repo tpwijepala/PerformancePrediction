48d53dd39da3be15ff8b37f558153ad5bfa4ee27
tpwijepala <tpwijepala@gmail.com>
2022-12-20 01:47:30 -0800

    Found Correlations

--- StudentPrediction.py
+++ StudentPrediction.py
@@ -4,5 +4,15 @@ import pandas as pd
 dataFrameM = pd.read_csv('studentData/student-mat.csv');
 dataFrameP = pd.read_csv('studentData/student-por.csv');
 
+print(dataFrameM)
 print(dataFrameM.corr())
+# Data shows a 0.80 corr between G1 & G3
+# Data shows a 0.90 corr between G2 & G3
+# Other Correlations are < 0.5
+
+
+print(dataFrameP)
 print(dataFrameP.corr())
+# Data shows a 0.83 corr between G1 & G3
+# Data shows a 0.92 corr between G2 & G3
+# Other Correlations are < 0.5
